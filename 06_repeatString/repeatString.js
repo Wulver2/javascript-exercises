ed22c17db9a12e4311165d982e923e53468e523d
Wulver2 <ross.shayla21@gmail.com>
2025-06-03 10:57:10 -0400

    completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(repeat, num) {
+    newString = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < num; i++) {
+        newString += repeat;
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
