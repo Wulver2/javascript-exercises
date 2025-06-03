85ba7c9fb672a4c9c0264c472bbbf7fa201cd5d7
Wulver2 <ross.shayla21@gmail.com>
2025-06-03 11:25:16 -0400

    completed exercises 7 and 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 1; i < arguments.length; i++) {
+        console.log(arguments[i]);
+        let remove = arguments[i];
+        while(arr.indexOf(remove) != -1) {
+            index = arr.indexOf(remove);
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
