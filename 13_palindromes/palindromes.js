c48c1eef0a4419c27b6653fd5105eca54efe043a
Wulver2 <ross.shayla21@gmail.com>
2025-06-05 12:53:41 -0400

    completed exercise 16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //could have set valid characters to alphanumeric instead
+    string = string.replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, '');
+    string = string.toLowerCase();
+    let reverse = string.split("").reverse().join("");
+    if(reverse.trim() == string.trim()) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
