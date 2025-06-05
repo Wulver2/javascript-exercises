3b2a83fda7a8cf1791efa363f8e47aa02a48b0b2
Wulver2 <ross.shayla21@gmail.com>
2025-06-05 12:40:04 -0400

    exercise 12 completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let f = 1;
+	for(let i = 2; i < num + 1; i++ ) {
+    f = f * i;
+  }
+  return f;
 };
 
 // Do not edit below this line
